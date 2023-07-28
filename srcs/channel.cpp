13253e2a6cb0bada1634439f28c8327700ba5b16
Alexandre VILLAR <avillar@paul-f3Br5s4.clusters.42paris.fr>
2023-07-28 10:34:56 +0200

    change mode

--- srcs/channel.cpp
+++ srcs/channel.cpp
@@ -4,7 +4,7 @@ channel::channel() : _limit(-1)
 {}
 
 channel::channel(std::string name, user *user, serveur *serv): _name(name), _serv(serv), _mode(""), _key(""){
-    add_user(user, 1);
+    add_user(user, 2);
 	_limit = -1;
 }
 
@@ -27,6 +27,16 @@ void channel::add_user(user *user, int mode)
 	user->_chan_name.push_back(_name);
 	if (mode == 1)
     {
+		std::string str = ":" + user->_nick + "!" + _serv->_name + "@localhost" + " JOIN " + _name + "\r\n";
+		// if (_users.size() == 1)
+			broadcast(str, "");
+		// else
+			// broadcast(str, user->_nick);
+		list_users();
+	}
+	if (mode == 2)
+    {
+		_operators.push_back(user);
 		std::string str = ":" + user->_nick + "!" + _serv->_name + "@localhost" + " JOIN " + _name + "\r\n";
 		// if (_users.size() == 1)
 			broadcast(str, "");
@@ -103,54 +113,63 @@ int channel::getMode(char c)
 	return (0);
 }
 
-// void channel::addMode(char c, std::string param)
-// {
-// /*	if (c == 'l')
-// 		setLimit(atoi(param.c_str()));*/
-// 	if (c == 'k')
-// 		setKey(param);
-// 	if (c == 'o')
-// 	{
-// 		for (std::vector<user *>::iterator it = _users.begin(); it != _users.end(); it++)
-// 		{
-// 			if ((*it)->_nick == param)
-// 			{
-// 				add_operator(*it);
-// 				return ;
-// 			}
-// 		}
-// 	}
-// 	else
-// 	{
-// 		if (c == 'l')
-// 		{
-// 			deleteMode('l', param);
-// 			setLimit(atoi(param.c_str()));
-// 		}
-// 		_mode.push_back(c);
-// 	}
-// }
+void channel::addMode(char c, std::string param)
+{
+	if (c == 'l')
+		setLimit(atoi(param.c_str()));
+	else if (c == 'k')
+		setKey(param);
+	else if (c == 'o')
+	{
+		for (std::vector<user *>::iterator it = _users.begin(); it != _users.end(); it++)
+ 		{
+			if ((*it)->_nick == param)
+			{
+ 				add_operator(*it);
+ 				return ;
+ 			}
+ 		}
+ 	}
+ 	else
+	{
+ 		if (c == 'l')
+		{
+ 			deleteMode('l', param);
+ 			setLimit(atoi(param.c_str()));
+ 		}
+ 		_mode.push_back(c);
+	}
+}
 
-// void channel::deleteMode(char mode, std::string param)
-// {
-// 	if (mode == 'l')
-// 		removeLimit();
-// 	if (mode == 'k')
-// 		deleteKey();
-// 	if (mode == 'o')
-// 	{
-// 		for (std::vector<user *>::iterator it = _operators.begin(); it != _operators.end(); it++)
-// 			if ((*it)->_nick == param)
-// 			{
-// 				delete_operator(*it);
-// 				return ;
-// 			}
-// 	}
-// 	else
-// 	{
-// 		_mode.erase(_mode.find(mode,1));
-// 	}
-// }
+void channel::deleteMode(char mode, std::string param)
+{
+	std::cout << "delete mode " << mode << std::endl;
+	if (mode == 'l')
+	{
+ 		removeLimit();
+		if (_mode.find(mode) != std::string::npos)
+			_mode.erase(_mode.find(mode), 1);
+	}
+	else if (mode == 'k')
+	{
+ 		deleteKey();
+		if (_mode.find(mode) != std::string::npos)
+			_mode.erase(_mode.find(mode), 1);
+	}
+	else if (mode == 'o')
+	{
+		for (std::vector<user *>::iterator it = _operators.begin(); it != _operators.end(); it++)
+ 			if ((*it)->_nick == param)
+ 			{
+ 				delete_operator(*it);
+ 				return ;
+			}
+	}
+	else
+	{
+ 		_mode.erase(_mode.find(mode,1));
+	}
+}
 
 void channel::setLimit(int limit)
 {
@@ -219,7 +238,7 @@ int channel::checkOper(std::string name)
 	return (0);
 }
 
-int channel::change_channel_mode(char sign, char mode, std::vector<std::string> command){
+/*int channel::change_channel_mode(char sign, char mode, std::vector<std::string> command){
 
 	std::string modes = "itkol";
 	std::cout << "mode : " << mode << std::endl;
@@ -250,4 +269,4 @@ int channel::change_channel_mode(char sign, char mode, std::vector<std::string> 
 		return (1);
 	}
 	return (0);
-}
+}*/
